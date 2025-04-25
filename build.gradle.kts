67ced0dbe0b258627a64853533b9aa36669abbec
tony_all <admin@minicraft.top>
2025-04-25 19:49:51 +0800

    update gradle update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,22 @@
 plugins {
-    id("io.freefair.lombok") version "6.5.0.3"
+    id("io.freefair.lombok") version "8.13.1"
     java
 }
 
 repositories {
     mavenLocal()
-    maven("http://maven.elmakers.com/repository/") {
-        isAllowInsecureProtocol = true
-    }
+    mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.tabooproject.org/repository/releases/")
     maven("https://jitpack.io")
-    maven("https://repo.maven.apache.org/maven2/")
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
-    compileOnly("org.bukkit:craftbukkit:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT") {
+        // As it's no longer available in spigot repository
+        exclude("net.md-5","bungeecord-chat")
+    }
+    implementation("ink.ptms.core:v10800:10800")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
 }
 
